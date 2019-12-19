ed612dc717dc5b13ecdd11300bc39ce276c6bb13
Murageshgouda Patil <murageshgoudapatil@localhost.localdomain>
2019-12-19 17:05:21 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Hrishikesh Patil & Akhilesh Patil, Son of Ashwini & Murageshgouda Patil
         </p>
         <a
           className="App-link"
